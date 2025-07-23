865e42b014d21305e36ab8a9f7cf7d78f63107f7
Hugo Virgen <virgenherrera@gmail.com>
2025-07-23 16:28:56 -0600

    chore: initialize pnpm workspace and TypeScript configuration
    - Added pnpm-workspace.yaml to define package structure for lib, lambdas, and web directories.
    - Created tsconfig.base.json with TypeScript compiler options and paths for module resolution.

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+// eslint.config.mjs
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import globals from 'globals'
+
+export default tseslint.config(
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      'eol-last': ['error', 'always'],
+      'linebreak-style': ['error', 'unix'],
+      'max-len': ['error', 150],
+      'newline-before-return': 'error',
+      'no-multiple-empty-lines': ['error']
+    }
+  }
+)
